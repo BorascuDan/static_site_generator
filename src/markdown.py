72800e770e457cdcbcbb4182af65e26cd4309c36
Borascu Dan <aperage@192.168.1.134>
2026-01-10 21:07:33 +0200

    feat: block type

--- src/markdown.py
+++ src/markdown.py
@@ -1,3 +1,16 @@
+from enum import Enum
+import re
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered list"
+    ORDERED_LIST = "ordered list"
+
+
 def markdown_to_blocks(markdow):
     blocks = markdow.split("\n\n")
 
@@ -10,3 +23,22 @@ def markdown_to_blocks(markdow):
         formated_blocks.append(striped)
 
     return formated_blocks
+
+
+def block_to_block_type(block):
+    if re.match(r"^#{1,6}\s+", block):
+        return BlockType.HEADING
+    if block.startswith("```\n") and block.endswith("```"):
+        return BlockType.CODE
+    if re.match(r"^(?:> .*)(?:\n> .*)*$", block):
+        return BlockType.QUOTE
+    if re.match(r"^(?:- .*)(?:\n- .*)*$", block):
+        return BlockType.UNORDERED_LIST
+
+    lines = block.splitlines()
+    for i, line in enumerate(lines, start=1):
+        prefix = f"{i}. "
+        if not line.startswith(prefix):
+            return BlockType.PARAGRAPH
+
+    return BlockType.ORDERED_LIST
