02679a3cca515fcff931490071d346b797a5cfd0
BorascuDan <aperage@192.168.1.134>
2026-01-11 09:29:21 +0200

    feat: copy static to public

--- src/main.py
+++ src/main.py
@@ -1,9 +1,22 @@
-from textnode import *
+from textnode import TextType, TextNode
+import os
+import shutil
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+public_path = os.path.join(BASE_DIR, "..", "public")
+static_path = os.path.join(BASE_DIR, "..", "static")
+
+
+def copy_static_to_public():
+    if os.path.exists(public_path):
+        shutil.rmtree(public_path)
+    shutil.copytree(static_path, public_path)
+
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    copy_static_to_public()
 
-    print(node)
 
 if __name__ == "__main__":
     main()
